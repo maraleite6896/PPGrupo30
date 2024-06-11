8c90fd88facc3d7e6f83db049c76cc835cecdb0c
Duarte Pires <orlandoduartepires@gmail.com>
2024-06-12 00:24:47 +0100

    Update Fire Department Class

--- projeto1/src/PP_GP30/project/FireDepartmentClass.java
+++ projeto1/src/PP_GP30/project/FireDepartmentClass.java
@@ -65,22 +65,22 @@ public class FireDepartmentClass implements FireDepartment{
     public boolean addAmbulance(Ambulance amblnc) throws AmbulanceException {
         
         if (ambulances == null) {
-        throw new AmbulanceException("The ambulance is null.");
-    }
-    
-    for (Ambulance existingAmbulance : ambulances) {
-        if (existingAmbulance.equals(amblnc)) {
-            return false; // Ambulance already exists
+            throw new AmbulanceException("The ambulance is null.");
         }
-    }
     
-    // Add the ambulance to the collection of ambulances
-    Ambulance[] newAmbulances = new Ambulance[ambulances.length + 1];
-    System.arraycopy(ambulances, 0, newAmbulances, 0, ambulances.length);
-    newAmbulances[ambulances.length] = amblnc;
-    ambulances = newAmbulances;
+        for (Ambulance existingAmbulance : ambulances) {
+            if (existingAmbulance.equals(amblnc)) {
+                return false; // Ambulance already exists
+            }
+        }
     
-    return true; // Ambulance was inserted successfully
+        // Add the ambulance to the collection of ambulances
+        Ambulance[] newAmbulances = new Ambulance[ambulances.length + 1];
+        System.arraycopy(ambulances, 0, newAmbulances, 0, ambulances.length);
+        newAmbulances[ambulances.length] = amblnc;
+        ambulances = newAmbulances;
+
+        return true; // Ambulance was inserted successfully
        
     }
 
@@ -89,115 +89,166 @@ public class FireDepartmentClass implements FireDepartment{
        return dailyPlans;
     }
 
+    /**
+     * 
+     * @param ldt data de inicio
+     * @param ldt1 data do fim
+     * @return em caso de sucesso retorna a lista de daily plans que existem 
+     * entre as duas datas, caso as datas não consistem num intervalo de tempo 
+     * válido retona null
+     */
     @Override
     public DailyPlan[] getDailyPlans(LocalDateTime ldt, LocalDateTime ldt1) {
-        return dailyPlans;
+       
+        if(ldt1.isBefore(ldt) && ldt.isAfter(ldt1)){
+            return null;
+        }
+  
+        DailyPlan[] dailyPlan;
+        dailyPlan = this.getDailyPlans();
+        DailyPlan[] temp = new DailyPlan[5];
+        int count = 0;
+        for(int i = 0; i < this.getDailyPlans().length; i++){
+            //a data tem de estar dentro estas duas datas
+            if(count == temp.length){
+                temp = (DailyPlan[])increaseArraySizeByFive(temp);
+            }
+            if( !dailyPlan[i].getDate().isBefore(ldt) && !dailyPlan[i].getDate().isAfter(ldt1)){
+                temp[count] = dailyPlan[i];
+                count++;
+            }
+        }
+        
+        return temp;
     }
 
+    /**
+     * 
+     * @return o Daily Plan mais recente
+     * @throws DailyPlanException e lançada esta exceção caso não existem Daily plans
+     */
     @Override
     public DailyPlan getDailyPan() throws DailyPlanException {
-        return dailyPan;
+        DailyPlan[] temp = this.getDailyPlans();
+        DailyPlan dailyPlan = null;
+        int count = 0;
+        
+        if(this.getDailyPlans().length == 0){
+            throw new DailyPlanException("Não existem DailyPlans");
+        }
+        
+        for(int i = 0; i < this.getDailyPlans().length; i++){
+            if(count == 0){
+                dailyPlan = temp[i];
+                count++;
+            }
+            if(temp[i].getDate().isAfter(dailyPlan.getDate())){
+                dailyPlan = temp[i];
+            }
+        }
+        
+        return dailyPlan;
     }
 
     @Override
     public boolean addDailyPlan(DailyPlan dp) throws DailyPlanException {
-        if (dailyPlans == null) {
-        throw new DailyPlanException("The daily plan is null.");
-    }
-    
-    // Verifica se a data do plano diário é anterior à data do plano diário mais recente
-    if (dailyPlans != null && dailyPlans.length > 0) {
-        DailyPlan mostRecentPlan = dailyPlans[dailyPlans.length - 1];
-        if (dp.getDate().isBefore(mostRecentPlan.getDate())) {
-            throw new DailyPlanException("The daily plan date is older than the most recent daily plan.");
+        if (dailyPlans == null || dp.getDate().isBefore(this.getDailyPan().getDate())) {
+            throw new DailyPlanException("The daily plan is null.");
         }
-    }
-    
-    // Verifica se o plano diário já existe na coleção de planos diários
-    for (DailyPlan existingPlan : dailyPlans) {
-        if (existingPlan.equals(dp)) {
-            return false; // Plano diário já existe
+
+        // Verifica se o plano diário já existe na coleção de planos diários
+        for (DailyPlan existingPlan : dailyPlans) {
+            if (existingPlan.equals(dp)) {
+                return false; // Plano diário já existe
+            }
         }
-    }
-    
-    // Adiciona o plano diário à coleção de planos diários
-    DailyPlan[] newDailyPlans = new DailyPlan[dailyPlans.length + 1];
-    System.arraycopy(dailyPlans, 0, newDailyPlans, 0, dailyPlans.length);
-    newDailyPlans[dailyPlans.length] = dp;
-    dailyPlans = newDailyPlans;
-    
-    return true; // Plano diário introduzido  
+
+        // Adiciona o plano diário à coleção de planos diários
+        DailyPlan[] newDailyPlans = new DailyPlan[dailyPlans.length + 1];
+        System.arraycopy(dailyPlans, 0, newDailyPlans, 0, dailyPlans.length);
+        newDailyPlans[dailyPlans.length] = dp;
+        dailyPlans = newDailyPlans;
+
+        return true; // Plano diário introduzido  
     }
 
     @Override
     public boolean addClient(Client client) throws ClientException {
         
         if (client == null) {
-        throw new ClientException("The client is null.");
-    }
-    
-    // Verifica se o cliente já existe na coleção de clientes
-    for (Client existingClient : clients) {
-        if (existingClient.equals(client)) {
-            return false; // Cliente já existe
+            throw new ClientException("The client is null.");
         }
-    }
     
-    // Adiciona o cliente à coleção de clientes
-    Client[] newClients = new Client[clients.length + 1];
-    System.arraycopy(clients, 0, newClients, 0, clients.length);
-    newClients[clients.length] = client;
-    clients = newClients;
+        // Verifica se o cliente já existe na coleção de clientes
+        for (Client existingClient : clients) {
+            if (existingClient.equals(client)) {
+                return false; // Cliente já existe
+            }
+        }
     
-    return true; // Cliente foi inserido com sucesso
+        // Adiciona o cliente à coleção de clientes
+        Client[] newClients = new Client[clients.length + 1];
+        System.arraycopy(clients, 0, newClients, 0, clients.length);
+        newClients[clients.length] = client;
+        clients = newClients;
+
+        return true; // Cliente foi inserido com sucesso
     }
 
     @Override
     public boolean addService(Service srvc) throws ServiceException {
         
         if (srvc == null) {
-        throw new ServiceException("The service is null.");
-    }
-    
-    // Verifica se o serviço já existe na coleção de serviços
-    for (Service existingService : services) {
-        if (existingService.equals(srvc)) {
-            return false; // Serviço já existe
+            throw new ServiceException("The service is null.");
         }
-    }
     
-    // Verifica se o cliente associado ao serviço existe
-    boolean clientExists = false;
-    for (Client existingClient : clients) {
-        if (existingClient.equals(srvc.getClient())) {
-            clientExists = true;
-            break;
+        // Verifica se o serviço já existe na coleção de serviços
+        for (Service existingService : services) {
+            if (existingService.equals(srvc)) {
+                return false; // Serviço já existe
+            }
         }
-    }
-    if (!clientExists) {
-        throw new ServiceException("The client associated with the service does not exist.");
-    }
-    
-    // Verifica se as patologias associadas ao serviço existem
-    boolean pathologyExists = false;
-    for (Pathology servicePathology : srvc.getPathologies()) {
-        if (servicePathology != null) {
-            pathologyExists = true;
-            break;
-        }
-    }
-    if (!pathologyExists) {
-        throw new ServiceException("A patologia não existe.");
-    }
 
+        // Verifica se o cliente associado ao serviço existe
+        boolean clientExists = false;
+        for (Client existingClient : clients) {
+            if (existingClient.equals(srvc.getClient())) {
+                clientExists = true;
+                break;
+            }
+        }
+        if (!clientExists) {
+            throw new ServiceException("The client associated with the service does not exist.");
+        }
+
+        // Verifica se as patologias associadas ao serviço existem
+        boolean pathologyExists = false;
+        for (Pathology servicePathology : srvc.getPathologies()) {
+            if (servicePathology != null) {
+                pathologyExists = true;
+                break;
+            }
+        }
+           
+        if (!pathologyExists) {
+            throw new ServiceException("A patologia não existe.");
+        }
+
+
+        // Adiciona o serviço à coleção de serviços
+        Service[] newServices = new Service[services.length + 1];
+        System.arraycopy(services, 0, newServices, 0, services.length);
+        newServices[services.length] = srvc;
+        services = newServices;
+
+        return true; // Serviço foi inserido com sucesso
+    }  
     
-    // Adiciona o serviço à coleção de serviços
-    Service[] newServices = new Service[services.length + 1];
-    System.arraycopy(services, 0, newServices, 0, services.length);
-    newServices[services.length] = srvc;
-    services = newServices;
-    
-    return true; // Serviço foi inserido com sucesso
+    public static Object[] increaseArraySizeByFive(Object[] originalArray) {
+        int newSize = originalArray.length + 5;
+        Object[] newArray = new Object[newSize];
+        System.arraycopy(originalArray, 0, newArray, 0, originalArray.length);
+
+        return newArray;
     }
-    
 }
